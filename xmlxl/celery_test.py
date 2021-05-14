fe926e00a756fd89b7f2bc6aaa5703e9e24c3ff9
snehalss <snehal.shah@gmail.com>
2021-05-14 05:46:36 +0000

    Minor Changes

--- xmlxl/celery_test.py
+++ xmlxl/celery_test.py
@@ -11,5 +11,9 @@ def test_task(n1, n2):
     logger.info("Adding two numbers")
     return n1 + n2
     
-
+# Linux commands to run:
 # celery -A xmlxl.celery_test:celery worker --loglevel=INFO -f LOGFILE --logfile=LOGFILE
+# flask run --host=0.0.0.0
+# sudo rabbitmq-server -detched
+# systemctl status rabbitmq-server.service
+
